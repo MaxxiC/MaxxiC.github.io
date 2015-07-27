783cc7d84e3197b7d0aa393f99906f754acad69e
MaxxiC <andmassic@icloud.com>
2015-07-28 01:48:56 +0200

    ok

--- packages.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-dpkg-deb -bZgzip projects/DurararaGlyph debs
-dpkg-deb -bZgzip projects/SoulectionGlyph debs
-#dpkg-deb -bZgzip projects/ debs
-#dpkg-deb -bZgzip projects/ debs
-#dpkg-deb -bZgzip projects/ debs
-#dpkg-deb -bZgzip projects/ debs
-#dpkg-deb -bZgzip projects/ debs
-#dpkg-deb -bZgzip projects/ debs
