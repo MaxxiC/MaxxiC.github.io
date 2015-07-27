0425c26c3448f0a39f1507824c1aaf9bfacea930
MaxxiC <andmassic@icloud.com>
2015-07-28 01:17:19 +0200

    ok

--- packages.sh
+++ packages.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-dpkg-deb -bZgzip projects/prova debs
-#
+#dpkg-deb -bZgzip projects/ debs
+#dpkg-deb -bZgzip projects/ debs
+#dpkg-deb -bZgzip projects/ debs
+#dpkg-deb -bZgzip projects/ debs
+#dpkg-deb -bZgzip projects/ debs
+#dpkg-deb -bZgzip projects/ debs
+#dpkg-deb -bZgzip projects/ debs
+#dpkg-deb -bZgzip projects/ debs
